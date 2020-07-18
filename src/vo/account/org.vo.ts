b065232ac5fb58bc5517206f0c9a4abc841cb3cd
vridhi <vridhi@samiteon.com>
2020-07-18 19:13:35 +0530

    gitignore update

--- src/vo/account/org.vo.ts
+++ src/vo/account/org.vo.ts
@@ -1,79 +1,88 @@
-import {AddressVo} from '../address.vo';
-import {OrgBrVo} from './org-br.vo';
-import {PosOrgVo} from "../pos";
-
-// Organization / Enterprise / Merchant / Business
+import { AddressVo } from '../address.vo';
+import { CompanyVo } from './company.vo';
+import { MerchantVo } from './merchant.vo';
+import { BankVo } from './bank.vo';
+import { OrgBrVo } from './org-br.vo';
+import { PosOrgVo } from "../pos";
 export interface OrgVo {
-  id: string;// orgId
-
-  // Corporate Legal Information
-  name: string; // Legal Name
-  type: string; // Type of Business
-  subType: string; //affiliationType --> BLACK_CAR | FLEET | TAXI | GIG |
-
-  taxpayerId: string //taxpayer Id
-
-  logo: string;
-  cover: string;
-
-  address: AddressVo;
-
-  domain: string;
-  tagline: string;//Business Tagline
-
-// DBA Information
-  dba: string; // DBA Name - Doing Business As / Trade Name
-  dbaAddress: AddressVo;
-
-  // Admin Information
-  title: string;
-  fName: string;//First Name
-  mName: string;//Middle Name
-  lName: string;//Last Name
-  doB: Date;
-  gender: string;
-  ownership: number; //% Ownership
-
-  ph: string;
-  email: string;
-
-  // countStore: number;
-  // masterOrgBr: OrgBrVo;
-  masterBr: OrgBrVo;
-  brMap: { string: OrgBrVo }; //for master key will be master
-
-  // Business Details
-  descBiz: string;//Business Description
-  service: { string: boolean };
-
-  status: string; //affiliation/status - NOT_APPROVED, IN_PROGRESS (Application submit for review), APPROVED/REJECT (by Admin), LIVE/DEAD (by Admin), ACTIVE/INACTIVE (by org)
-  del: boolean;
-
-  modifiedBy: string;
-  createdBy: string;
-  modified: Date;
-  created: Date;
-
-  //Payment
-  stripeCustId: string; // cus_ and in strip_customer collection >> id >> copr_<corpId>
-  payntCustId: string;
-
-  stripeExpressAc: string;
-
-  // Limo specific field
-  // TODO Transfer to the limo specific object
-  veteran: boolean;
-  guncard: boolean;
-  backgroundCheck: boolean;
-  experience: number;
-  cellManager: string;
-  dispatch: string;
-
-  otherLicense: boolean;
-  dress: { string: boolean };
-  lang: { string: boolean };
-
-  // POS specific field
-  pos: PosOrgVo;
-  //ride: RideOrgVo;
+    company: CompanyVo;
+    merchant: MerchantVo;
+    bank: BankVo;
+    address: AddressVo;
+    paymentType:  string[]; 
+    maxTicketAmt: string;
+    maxMonthVolume: string;
+    driverLicenseNo: string;
+    driverLicenseState: string;
+    socialSecurityNo: string;
+    terminalId: string;
+    bankId: string;
+    industryCodeNo: string;
+    timeZone:string;
+    dayLightSaving: string;
+    accquirer: string;
+    accquirerContact: string;
+    accquirerPhn: string;
+    currencyCode: string;
+    merchantInnowiCheckoutCode: string;
+    countryCode: string;
+    branch: string;
+    chain: string;
+    debit: string;
+    credit: string;
+    billingAddress: AddressVo;
+    shippingAddress: AddressVo;
+    service:string;
+    storeUnderCompanyNo:string;
+    deviceno:string;
+    virtualUsername: string;
+    virtualPassword: string;
+    id: string;
+    name: string;
+    type: string;
+    subType: string;
+    taxpayerId: string;
+    logo: string;
+    cover: string;
+    domain: string;
+    tagline: string;
+    dba: string;
+    dbaAddress: AddressVo;
+    title: string;
+    fName: string;
+    mName: string;
+    lName: string;
+    doB: Date;
+    gender: string;
+    ownership: number;
+    ph: string;
+    email: string;
+    masterBr: OrgBrVo;
+    brMap: {
+        string: OrgBrVo;
+    };
+    descBiz: string;
+    status: string;
+    del: boolean;
+    modifiedBy: string;
+    createdBy: string;
+    modified: Date;
+    created: Date;
+    stripeCustId: string;
+    payntCustId: string;
+    stripeExpressAc: string;
+    veteran: boolean;
+    guncard: boolean;
+    backgroundCheck: boolean;
+    experience: number;
+    cellManager: string;
+    dispatch: string;
+    otherLicense: boolean;
+    dress: {
+        string: boolean;
+    };
+    lang: {
+        string: boolean;
+    };
+    pos: PosOrgVo;
 }
